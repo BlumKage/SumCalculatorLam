0479e36cad75989d20476412f3f1f8ff5240ded3
BlumKage <152303532+BlumKage@users.noreply.github.com>
2024-01-08 16:43:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a positive integer: "))
+sum = 0
+for counter in range(num + 1):
+    sum += counter
+print("The sum of all integers from 0 to", num, "is", sum)                     
